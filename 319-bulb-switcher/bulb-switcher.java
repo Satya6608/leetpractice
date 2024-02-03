8917786e533acfe40ee70e87d859e35b104146f4
Satya prakash <87296687+Satya6608@users.noreply.github.com>
2024-02-03 23:48:31 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (16.61%) - LeetSync

--- 319-bulb-switcher/bulb-switcher.java
+++ 319-bulb-switcher/bulb-switcher.java
@@ -1,5 +1,11 @@
 class Solution {
     public int bulbSwitch(int n) {
-        return ( int ) Math.sqrt(n);
+        int count = 0;
+        int i = 1;
+        while(i*i <= n){
+            count ++;
+            i++;
+        }
+        return count;
     }
 }
