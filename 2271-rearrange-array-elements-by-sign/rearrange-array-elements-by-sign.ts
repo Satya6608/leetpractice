780c157553ca7326928b9b0fdd7ed0215f91a12e
Satya prakash <Satyaprakash6608@gmail.com>
2024-02-14 22:16:35 +0530

    Time: 379 ms (17.57%) | Memory: 95.6 MB (62.16%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.ts
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.ts
@@ -1,16 +1,15 @@
 function rearrangeArray(nums: number[]): number[] {
-    let ans : number[];
-        ans =[nums.length];
-        let pos = 0, neg = 1;
-
-        for(let i = 0; i<nums.length; i++){
-            if(nums[i] > 0){
-                ans[pos] = nums[i];
-                pos +=2;
-            }else{
-                ans[neg] = nums[i];
-                neg +=2;
-            }
+    let posI = 0
+    let negI = 1
+    const output = new Array(nums.length)
+    nums.forEach(num => {
+        if (num > 0) {
+            output[posI] = num
+            posI += 2
+        } else if (num < 0) {
+            output[negI] = num
+            negI += 2
         }
-        return ans;
+    })
+    return output
 };
