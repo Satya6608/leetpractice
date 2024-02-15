b61ab494bdd9d3b7d20b66b4f05f60472b6c7bf6
Satya prakash <Satyaprakash6608@gmail.com>
2024-02-15 20:12:43 +0530

    Time: 3 ms (86.67%) | Memory: 43 MB (84.42%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,27 +1,25 @@
 class Solution {
-    static List<List<Integer>> ans;
-    static void helper(int arr[], int idx, List<Integer> a, int target){
-        if(target==0){
-            System.out.println(a);
-            ans.add(new ArrayList<>(a));
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+       List<Integer> list = new ArrayList<>();
+        Arrays.sort(candidates);
+        List<List<Integer>> res = new ArrayList<>();
+        dfs(0, target, candidates, list, res);
+
+        return res;
+    }
+
+    public static void dfs(int index, int target, int[] nums, List<Integer> list, List<List<Integer>> res) {
+        if (target == 0) {
+            res.add(new ArrayList<>(list));
             return;
         }
-        if(idx==arr.length) return;
-        
-        if(arr[idx]<=target){
-            a.add(arr[idx]);
-            helper(arr,idx+1,a,target-arr[idx]);
-            a.remove(a.size()-1);
-        }
-        int j = idx+1;
-        while(j<arr.length && arr[j]==arr[j-1]) j++;  // avoiding duplicates
-        helper(arr,j,a,target); 
-    }
-    public List<List<Integer>> combinationSum2(int[] arr, int target) {
-        Arrays.sort(arr);
-        List<Integer> a = new ArrayList<>();
-        ans = new ArrayList<>();
-        helper(arr,0,a,target);
-        return ans;
+
+        for (int i = index; i < nums.length; i++) {
+            if (i > index && nums[i] == nums[i - 1]) continue;
+            if (nums[i] > target) break;
+            list.add(nums[i]);
+            dfs(i + 1, target -nums[i], nums, list, res);
+            list.remove(list.size() - 1);
+        } 
     }
 }
