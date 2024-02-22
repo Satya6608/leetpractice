504a55fcaa12927e80bf3f04050d081220d540c4
Satya prakash <Satyaprakash6608@gmail.com>
2024-02-22 23:56:38 +0530

    without using built in

--- Js/is_palindrome.js
+++ Js/is_palindrome.js
@@ -5,3 +5,16 @@ function isPalindrome(s){
 }
 let str = 'racecar'
 console.log(isPalindrome(str))
+
+// second without using built in 
+
+function isPalindromewihout(){
+    for (let i = 0; i < Math.floor(s.length / 2); i++) {
+        if (s[i] !== s[s.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(isPalindromewihout(str))
