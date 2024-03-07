6cd42dc53d760698c5079a021c0e45521ac2b9d0
Satya prakash <Satyaprakash6608@gmail.com>
2024-03-07 23:31:22 +0530

    Time: 49 ms (86.29%) | Memory: 49.1 MB (55.16%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+     let ans = '';
+
+        strs.sort();
+        let first = strs[0];
+        let last = strs[strs.length - 1];
+
+        for(let i = 0; i < Math.min(first.length, last.length); i++){
+          if(first.charAt(i) != last.charAt(i)) return ans.toString();
+          ans += first.charAt(i);
+        }
+
+        return ans.toString();
+};
