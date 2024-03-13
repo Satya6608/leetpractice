76650dd9e79ae05f8c3ad35534b93912a3e78b41
Satya prakash <Satyaprakash6608@gmail.com>
2024-03-13 23:14:34 +0530

    Time: 10 ms (100.00%) | Memory: 44.5 MB (95.95%) - LeetSync

--- 993-tallest-billboard/tallest-billboard.java
+++ 993-tallest-billboard/tallest-billboard.java
@@ -1,24 +1,21 @@
 class Solution {
-
-    int rec(int [] rod, int diff, int idx, int [][]dp){
-      if(idx<0){
-        if(diff == 0) return 0;
-        return Integer.MIN_VALUE;
-      }
-      if(dp[idx][diff+5000] != -1) return dp[idx][diff+5000];
-
-      int notpick = rec(rod, diff, idx-1, dp);
-      int pick1 = rod[idx] + rec(rod, diff+rod[idx],idx-1,dp);
-      int pick2 = rec(rod,diff-rod[idx], idx-1, dp);
-
-      dp[idx][diff+5000] = Math.max(notpick, Math.max(pick1, pick2));
-
-      return dp[idx][diff+5000]; 
-    }
-
     public int tallestBillboard(int[] rods) {
-        int [][] dp = new int [rods.length][10001];
-        for(int i=0; i<rods.length;i++) Arrays.fill(dp[i], -1);
-        return rec(rods, 0, rods.length-1, dp);
+        int sum = Arrays.stream(rods).sum();
+        int[] dp = new int[sum + 1];
+        Arrays.fill(dp, -1);
+        dp[0] = 0;
+
+        for (int rod : rods) {
+            int[] dp2 = dp.clone();
+            for (int i = 0; i + rod <= sum; i++) {
+                if (dp[i] >= 0) {
+                    dp2[i + rod] = Math.max(dp2[i + rod], dp[i]);
+                    dp2[Math.abs(i - rod)] = Math.max(dp2[Math.abs(i - rod)], dp[i] + Math.min(i,rod));
+                }
+            }
+            dp = dp2;
+        }
+
+        return dp[0];
     }
 }
