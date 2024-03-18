ff52cefe84b8770b33acd7a1646eb88a5bf0d67a
satya <satyaprakash6608@gmail.com>
2024-03-18 22:41:11 +0530

    format some code

--- Java_dsa/square-a-sorted-array.java
+++ Java_dsa/square-a-sorted-array.java
@@ -1,53 +1,50 @@
-
-
-
 class Solution {
-    public int getMax(int[] arr) {
-            int max = arr[0];
-                    for (int i = 1; i < arr.length; i++) {
-                                if (arr[i] > max) {
-                                                max = arr[i];
-                                                            }
-                                                                    }
-                                                                            return max;
-                                                                                }
 
-                                                                                    public void countSort(int[] arr, int exp) {
-                                                                                            int[] output = new int[arr.length];
-                                                                                                    int[] count = new int[10];
+  public int getMax(int[] arr) {
+    int max = arr[0];
+    for (int i = 1; i < arr.length; i++) {
+      if (arr[i] > max) {
+        max = arr[i];
+      }
+    }
+    return max;
+  }
 
-                                                                                                            for (int i = 0; i < arr.length; i++) {
-                                                                                                                        count[(arr[i] / exp) % 10]++;
-                                                                                                                                }
+  public void countSort(int[] arr, int exp) {
+    int[] output = new int[arr.length];
+    int[] count = new int[10];
 
-                                                                                                                                        for (int i = 1; i < 10; i++) {
-                                                                                                                                                    count[i] += count[i - 1];
-                                                                                                                                                            }
+    for (int i = 0; i < arr.length; i++) {
+      count[(arr[i] / exp) % 10]++;
+    }
 
-                                                                                                                                                                    for (int i = arr.length - 1; i >= 0; i--) {
-                                                                                                                                                                                output[count[(arr[i] / exp) % 10] - 1] = arr[i];
-                                                                                                                                                                                            count[(arr[i] / exp) % 10]--;
-                                                                                                                                                                                                    }
+    for (int i = 1; i < 10; i++) {
+      count[i] += count[i - 1];
+    }
 
-                                                                                                                                                                                                            for (int i = 0; i < arr.length; i++) {
-                                                                                                                                                                                                                        arr[i] = output[i];
-                                                                                                                                                                                                                                }
-                                                                                                                                                                                                                                    }
+    for (int i = arr.length - 1; i >= 0; i--) {
+      output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+      count[(arr[i] / exp) % 10]--;
+    }
 
-                                                                                                                                                                                                                                        public void radixSort(int[] arr) {
-                                                                                                                                                                                                                                                int max = getMax(arr);
+    for (int i = 0; i < arr.length; i++) {
+      arr[i] = output[i];
+    }
+  }
 
-                                                                                                                                                                                                                                                        for (int exp = 1; max / exp > 0; exp *= 10) {
-                                                                                                                                                                                                                                                                    countSort(arr, exp);
-                                                                                                                                                                                                                                                                            }
-                                                                                                                                                                                                                                                                                }
+  public void radixSort(int[] arr) {
+    int max = getMax(arr);
 
-                                                                                                                                                                                                                                                                                    public int[] sortedSquares(int[] nums) {
-                                                                                                                                                                                                                                                                                            for (int i = 0; i < nums.length; i++) {
-                                                                                                                                                                                                                                                                                                        nums[i] = nums[i] * nums[i];
-                                                                                                                                                                                                                                                                                                                }
-                                                                                                                                                                                                                                                                                                                        radixSort(nums);
-                                                                                                                                                                                                                                                                                                                                return nums;
-                                                                                                                                                                                                                                                                                                                                    }
-                                                                                                                                                                                                                                                                                                                                    }
-                                                                                                                                                                                                                                                                                                                                    
+    for (int exp = 1; max / exp > 0; exp *= 10) {
+      countSort(arr, exp);
+    }
+  }
+
+  public int[] sortedSquares(int[] nums) {
+    for (int i = 0; i < nums.length; i++) {
+      nums[i] = nums[i] * nums[i];
+    }
+    radixSort(nums);
+    return nums;
+  }
+}
